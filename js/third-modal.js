7ed24893ddc98d358e21e1b48d983c2182868cd4
Danylo Altukhov <dan.97198@gmail.com>
2022-06-21 23:53:43 +0300

    сделал js под каждую модалку

--- js/third-modal.js
+++ js/third-modal.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector("[third-modal-open]"),
+    closeModalBtn: document.querySelector("[third-modal-close]"),
+    modal: document.querySelector("[third-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
@@ -12,6 +12,3 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
-
-
-
