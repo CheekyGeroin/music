035d41c18a2ecaafda9b0a244e63f1d474cdcceb
Danylo Altukhov <dan.97198@gmail.com>
2022-06-21 21:05:13 +0300

    сделал скрипт для модалки

---
+++ js/modal.js
@@ -0,0 +1,43 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[second-modal-open]"),
+    closeModalBtn: document.querySelector("[second-modal-close]"),
+    modal: document.querySelector("[second-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[third-modal-open]"),
+    closeModalBtn: document.querySelector("[third-modal-close]"),
+    modal: document.querySelector("[third-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
